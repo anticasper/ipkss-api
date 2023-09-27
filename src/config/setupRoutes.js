d188f9aaeea5a90f1e0bd40025ac4786d365ab88
Diovanni Souza <diovanni@anticasper.com>
2023-09-27 12:17:53 -0300

    feat: create setup routes alg

--- src/config/setupRoutes.js
+++ src/config/setupRoutes.js
@@ -1,4 +1,14 @@
+const fs = require('fs')
+
+const routePath = '../routes/'
+
 const loadRoutes = (app) => {
+  const fileList = fs.readdirSync('./src/routes')
+  for (let i = 0; i < fileList.length; i++) {
+    const file = fileList[i]
+    const routes = require(routePath + file)
+    app = routes.add(app)
+  }
   return app
 }
 
